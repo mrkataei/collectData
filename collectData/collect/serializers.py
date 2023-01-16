3b7a542e5e1de3e52f8fdd5341c7aaf09fdf3ebe
mrkataei <kouroshinfo96@gmail.com>
2023-01-16 17:31:01 +0330

    get 100 candles from bitfinex

---
+++ collectData/collect/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+
+class Candle(serializers.BaseSerializer):
+    pass
--- collectData/collect/views.py
+++ collectData/collect/views.py
@@ -2,15 +2,22 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+import requests
 
 
+
+BASE_URL = 'https://api-pub.bitfinex.com'
 
 @api_view(['GET'])
-@permission_classes((IsAuthenticated,))
+@permission_classes((AllowAny,))
 def candle(request):
     try:
-        user = "hello world!"
+        symbol = request.query_params['symbol']
+        timeframe = request.query_params['timeframe']
+
+        candles = requests.get(url = BASE_URL + f'/v2/candles/trade:{timeframe}:{symbol}/hist?limit=100').text
+
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
-        
-    return Response(user, status=status.HTTP_200_OK)
+
+    return Response(candles, status=status.HTTP_200_OK)
